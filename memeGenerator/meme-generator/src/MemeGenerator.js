2a173c18eb5745c9cc2327fc14e73e049c56097f
Anan <anan.w.ma@gmail.com>
2019-02-27 16:55:58 -0800

    Memegenerator
    - Header image and text
    - Random image selector from API
    - Form for text

---
+++ memeGenerator/meme-generator/src/MemeGenerator.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+class MemeGenerator extends Component {
+	
+	state = {
+		imgURL: "http://i.imgflip.com/1bij.jpg",
+		topText: "",
+		botText: "",
+		memeArray: [],
+	}
+
+	componentDidMount = () => {
+		fetch("https://api.imgflip.com/get_memes")
+			.then(result => result.json(result))
+			.then(result => {
+				this.setState({
+					memeArray: result.data.memes
+				})
+			})
+	}
+
+	handleChange = (e) => {
+		let {name, value} = e.target
+		this.setState({
+			[name]: value
+		})
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault()
+		let idx = Math.floor(Math.random() * 100)
+		this.setState({
+			imgURL: this.state.memeArray[idx].url
+		})
+	}
+
+  render() {
+    return (
+      <div>
+      	<form className="meme-form" onSubmit={this.handleSubmit}>
+      		<input name="topText" placeholder="top text" value={this.state.topText} onChange={this.handleChange}/> 
+      		<input name="botText" placeholder="bottom text" value={this.state.botText} onChange={this.handleChange}/>
+      		<button> Generate </button>
+      	</form>
+      	<div className="meme">
+	      	<img src={this.state.imgURL} alt="meme"/>
+	      	<h2 className="top"> {this.state.topText} </h2>
+	      	<h2 className="bottom"> {this.state.botText} </h2>
+	      </div>
+      </div>
+    )
+  }
+}
+
+export default MemeGenerator
